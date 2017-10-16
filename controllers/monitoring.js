4de8d3917362af3df1cfa80fd4cfd076e19546dd
David Heymann <heymann.david@gmail.com>
2017-10-16 16:46:37 +0300

    with promise

--- controllers/monitoring.js
+++ controllers/monitoring.js
@@ -5,29 +5,57 @@ var express = require('express')
     ,exec = require('ssh-exec');
 
 
-router.get('/', (req, res, next)=>{
-    exec(/* "vmstat 1 2|tail -1|awk '{print 100-$15}'" */ "cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS='' '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}'", 
-    'monitor@52.138.150.180', function (err, stdout, stderr, req) {
-        stdout = stdout.replace(/^\s+|\s+$/g, '');
-        console.log('err: '+ err +', stdout: '+ stdout+', stderr: '+stderr)
-        res.locals.cpu = stdout || "not cpu";
-        next()
-    })
-},(req ,res, next)=>{
-    exec(/* "free | grep Mem | awk '{print ($3+$6)*100/$2}'" */ "free | grep Mem | awk '{print ($2-$7)*100/$2}'", 
-    'monitor@52.138.150.180', function (err, stdout, stderr, req) {
-        stdout = stdout.replace(/^\s+|\s+$/g, '');
-        console.log('err: '+ err +', stdout: '+ stdout+', stderr: '+stderr)
-        res.locals.ram = stdout || "not ram";
-        next()
-    }) 
-},(req, res)=>{
-    console.log('send')
-    //res.send('cpu: '+res.locals.cpu+ ". ram: "+res.locals.ram)
-    res.render('index', { CPU: res.locals.cpu, RAM: res.locals.ram, SERVER_NAME: "monitor-server" });
-    
+router.get('/getCpu', (req, res)=>{
+    catchCpuFromServer('monitor', '52.138.150.180').then(
+        function(data){
+            res.send(data);
+        }
+    )    
+})
+
+router.get('/getRam', (req, res)=>{
+    catchRamFromServer('monitor', '52.138.150.180').then(
+        function(data){
+            res.send(data);
+        }
+    )    
 })
 
 
+router.get('/', (req, res)=>{
+    Promise.all([catchCpuFromServer('monitor', '52.138.150.180'), 
+        catchRamFromServer('monitor', '52.138.150.180')])
+        .then(
+            function(data){
+                console.log('send')
+                //res.send('cpu: '+res.locals.cpu+ ". ram: "+res.locals.ram)
+                res.render('index', { CPU: data[0], RAM: data[1], SERVER_NAME: "monitor-server" });
+            }
+        )    
+})
+
+function catchCpuFromServer(username, ipAddress){
+    return new Promise(function(resolve, reject){
+        exec(/* "vmstat 1 2|tail -1|awk '{print 100-$15}'" */ "cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS='' '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}'", 
+            username+'@'+ipAddress, function (err, stdout, stderr, req) {
+                stdout = stdout.replace(/^\s+|\s+$/g, '');
+                console.log('err: '+ err +', stdout: '+ stdout+', stderr: '+stderr)
+                var result = stdout || "not cpu"
+                resolve(result);
+        })
+    })
+}
+
+function catchRamFromServer(username, ipAddress){
+    return new Promise(function(resolve, reject){
+        exec(/* "free | grep Mem | awk '{print ($3+$6)*100/$2}'" */ "free | grep Mem | awk '{print ($2-$7)*100/$2}'", 
+        username+'@'+ipAddress, function (err, stdout, stderr, req) {
+            stdout = stdout.replace(/^\s+|\s+$/g, '');
+            console.log('err: '+ err +', stdout: '+ stdout+', stderr: '+stderr)
+            var result = stdout || "not ram";
+            resolve(result);
+        })
+    }) 
+}
+
 module.exports = router;
-;
